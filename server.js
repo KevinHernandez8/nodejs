3a803832e7fdbd54cee9137e0f513d6222271c3f
Kevin <kevin.hernandezr8@gmail.com>
2019-10-21 20:26:21 -0500

    Web sockets al enviar mensajes

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+
 const bodyParser = require('body-parser')
 const router = require('./network/routes')
 const db = require('./db')
+const socket = require('./socket')
 
 db('mongodb://localhost:27017/chat')
-var app = express()
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 router(app)
+socket.connect(server)
 
 // Express "auto-genera" las rutas para acceder a todos los archivos
 // dentro de la carpeta especificada
 app.use('/app', express.static('public'))
 
-app.listen(3000)
-console.log('La aplicación está escuchando en http://localhost:3000')
+server.listen(3000, () => {
+    console.log('La aplicación está escuchando en http://localhost:3000')
+})
